39ac4a8fe110e72a3530d3728929447ba1243cd9
X_Laptop <xandnea@gmail.com>
2023-02-07 20:59:09 -0800

    Working ?

--- CS32 HW2/eval.cpp
+++ CS32 HW2/eval.cpp
@@ -1,100 +1,3 @@
-int evaluate(string infix, const Set& trueValues, const Set& falseValues, string& postfix, bool& result) {
-    postfix = "";
-    stack<char> operatorStack;
-
-    for (int i = 0; i < infix.size(); i++) {
-        char ch = infix[i];
-
-        switch(ch)
-            case /*operand*/:
-                postfix += ch;
-                break;
-            case '(':
-                operatorStack.push(ch);
-                break;
-            case ')':
-                while (operatorStack.top() != '(') {
-                    postfix += operatorStack.top();
-                    operatorStack.pop();
-                }
-                operatorStack.pop();
-                break;
-            case /*operator*/:
-                while ((!operatorStack.empty()) && (operatorStack.top() != '(') && precedence of ch <= precedence of operatorStack.top()) {
-                    postfix += operatorStack.top();
-                    operatorStack.pop();
-                }
-                operatorStack.push(ch);
-                break;
-    }
-
-    while (!operatorStack.empty()) {
-        postfix += operatorStack.top();
-        operatorStack.pop();
-    }
-
-    stack<char> operandStack;
-    for (int i = 0; i < postfix.size(); i++) {
-        char ch = postfix[i];
-
-        if (isalpha(ch))
-            operandStack.push(ch);
-        else {
-            ch = postfix[i + 1];
-            operandStack.push(ch);
-            operandStack.pop();
-            ch = postfix[i - 1];
-            operandStack.push(ch);
-            operandStack.pop();
-
-        }
-    }
-
-
-                  And here is his pseudocode for the evaluation of the postfix expression :
-
-              Initialize the operand stack to empty
-                  For each character ch in the postfix string
-                  if ch is an operand
-                      push the value that ch represents onto the operand stack
-                  else // ch is a binary operator
-                      set operand2 to the top of the operand stack
-                      pop the stack
-                      set operand1 to the top of the operand stack
-                      pop the stack
-                      apply the operation that ch represents to operand1and
-                      operand2, and push the result onto the stack
-                      When the loop is finished, the operand stack will contain one item,
-                      the result of evaluating the expression
-}
-// Evaluate a boolean expression
-//   If infix is a syntactically valid infix boolean expression whose
-//   only operands are single lower case letters (whether or not they
-//   appear in the values sets), then postfix is set to the postfix
-//   form of the expression.  If not, postfix might or might not be
-//   changed, result is unchanged, and the function returns 1.
-//
-//   If infix is a syntactically valid infix boolean expression whose
-//   only operands are single lower case letters:
-//
-//      If every operand letter in the expression appears in either
-//      trueValues or falseValues but not both, then result is set to the
-//      result of evaluating the expression (using for each letter in the
-//      expression the value true if that letter appears in trueValues or
-//      false if that letter appears in false values) and the function
-//      returns 0.
-//
-//      Otherwise, result is unchanged and the value the function returns
-//      depends on these two conditions:
-//        at least one letter in the expression is in neither the
-//            trueValues nor the falseValues sets; and
-//        at least one letter in the expression is in both the
-//            trueValues and the falseValues set.
-//      If only the first condition holds, the function returns 2; if
-//      only the second holds, the function returns 3.  If both hold
-//      the function returns either 2 or 3 (and the function is not
-//      required to return the same one if called another time with the
-//      same arguments).
 
 #include "Set.h"  // with ItemType being a type alias for char
 #include <iostream>
@@ -103,6 +6,178 @@ int evaluate(string infix, const Set& trueValues, const Set& falseValues, string
 #include <cctype>
 #include <cassert>
 using namespace std;
+
+int evaluate(string infix, const Set& trueValues, const Set& falseValues, string& postfix, bool& result) {
+
+    string test = "";
+    string alphas = "";
+
+    if (infix.size() == 0)
+        return 1;
+
+    for (int i = 0; i < infix.size(); i++) {
+        char ch = infix[i];
+
+        if (isalpha(ch))
+            alphas += ch;
+
+        if ((isalpha(ch) && islower(ch)) || (ch == '(') || (ch == ')') || (ch == '!') || (ch == '&') || (ch == '|'))
+            test += ch;
+        else if (ch == ' ')
+            continue;
+        else
+            return 1;            
+    }
+
+    char curr;
+    char next;
+    bool parenthesis = true;
+
+    for (int i = 0; i < test.size(); i++) {
+        curr = test[i];
+        next = test[i + 1];
+
+        switch (curr) {
+            case '(':
+                if (isalpha(next) || (next == '!') || (next == '(')) {
+                    parenthesis = !parenthesis;
+                    break;
+                }
+                else
+                    return 1;
+            case ')':
+                if (isalpha(next) || (next == '!')) 
+                    return 1;
+                else {
+                    parenthesis = !parenthesis;
+                    break;
+                }
+            case '!':
+                if (isalpha(next) || next == '(')
+                    break;
+                else
+                    return 1;
+            case '&':
+            case '|':
+                if (isalpha(next) || (next == '!') || (next == '('))
+                    break;
+                else
+                    return 1;
+            default:                                                        // if character
+                if (isalpha(next) || (next == '!') || (next == '('))
+                    return 1;
+                else
+                    break;
+        }
+    }
+    
+    if (!parenthesis) // if parenthesis is false (there is an odd # of parenthesis) then return 1
+        return 1;
+
+    for (int i = 0; i < alphas.size(); i++) {
+        char ch = alphas[i];
+
+        if ((trueValues.contains(ch) || falseValues.contains(ch)) && !(trueValues.contains(ch) && falseValues.contains(ch))) // if ch appears in either true values or false values but not both
+            continue;
+        else {
+            if (!(trueValues.contains(ch) || falseValues.contains(ch)))
+                return 2;
+            if (trueValues.contains(ch) && falseValues.contains(ch))
+                return 3;
+        }
+    }
+
+    postfix = "";
+    stack<char> operatorStack;
+
+    for (int i = 0; i < infix.size(); i++) {
+        char ch = infix[i];
+
+        switch (ch) {
+        default:
+            postfix += ch;
+            break;
+        case ' ':
+            break;
+        case '(':
+            operatorStack.push(ch);
+            break;
+        case ')':
+            while (operatorStack.top() != '(') {
+                postfix += operatorStack.top();
+                operatorStack.pop();
+            }
+            operatorStack.pop();
+            break;
+        case '|':
+            while ((!operatorStack.empty()) && (operatorStack.top() != '(')) {
+                postfix += operatorStack.top();
+                operatorStack.pop();
+            }
+            operatorStack.push(ch);
+            break;
+        case '&':
+            while ((!operatorStack.empty()) && (operatorStack.top() != '(') && (operatorStack.top() != '|')) {
+                postfix += operatorStack.top();
+                operatorStack.pop();
+            }
+            operatorStack.push(ch);
+            break;
+        case '!':
+            while ((!operatorStack.empty()) && (operatorStack.top() != '(') && (operatorStack.top() != '&') && (operatorStack.top() != '|')) {
+                postfix += operatorStack.top();
+                operatorStack.pop();
+            }
+            operatorStack.push(ch);
+            break;
+        }
+    }
+
+    while (!operatorStack.empty()) {
+        postfix += operatorStack.top();
+        operatorStack.pop();
+    }
+
+
+    stack<bool> operandStack;
+    for (int i = 0; i < postfix.size(); i++) {
+        char ch = postfix[i];
+
+        if (isalpha(ch)) {
+            if ((falseValues.contains(ch)) && (postfix[i + 1] == '!'))
+                operandStack.push(true);
+            else if ((trueValues.contains(ch)) && (postfix[i + 1] == '!'))
+                operandStack.push(false);
+            else if (trueValues.contains(ch))
+                operandStack.push(true);
+            else if (falseValues.contains(ch))
+                operandStack.push(false);
+        }
+        else {
+            if (ch == '!')
+                continue;
+            bool operand2 = operandStack.top();
+            operandStack.pop();
+            bool operand1 = operandStack.top();
+            operandStack.pop();
+            switch (ch) {
+            case '&':
+                operandStack.push(operand1 && operand2);
+                break;
+            case '|':
+                operandStack.push(operand1 || operand2);
+                break;
+            }
+        }
+    }
+    result = operandStack.top();
+
+    //cerr << "returning: 0, pf: " << postfix << ", answer: " << result << endl;
+
+    return 0;
+}
+
+
 
 int main()
 {
@@ -114,6 +189,15 @@ int main()
         trues.insert(trueChars[k]);
     for (int k = 0; k < falseChars.size(); k++)
         falses.insert(falseChars[k]);
+
+ /*   string pf;
+    bool answer;
+    evaluate("f  |  !f & (t&n) ", trues, falses, pf, answer);
+    evaluate("!w&!t", trues, falses, pf, answer);
+    evaluate("!w|!t", trues, falses, pf, answer);
+    evaluate("(!t&f)|z", trues, falses, pf, answer);
+    cerr << "should be pf == ff!tn&&| and is: " << pf << endl;
+    cerr << answer << endl;*/
 
     string pf;
     bool answer;
